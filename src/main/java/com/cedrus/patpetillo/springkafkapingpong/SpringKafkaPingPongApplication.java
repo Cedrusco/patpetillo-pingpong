7abf35c83dc7318091073462d30565db8308d9ba
Pat Petillo <patpetillo@gmail.com>
2020-04-09 10:11:54 -0400

    Initial commit

---
+++ src/main/java/com/cedrus/patpetillo/springkafkapingpong/SpringKafkaPingPongApplication.java
@@ -0,0 +1,13 @@
+package com.cedrus.patpetillo.springkafkapingpong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaPingPongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaPingPongApplication.class, args);
+	}
+
+}
