3e0264e2d77a163436e2c3ec41591be65aade063
Pat Petillo <patpetillo@gmail.com>
2020-04-21 10:27:40 -0400

    remove unused

--- src/main/java/com/cedrus/patpetillo/springkafkapingpong/controller/PingPongBallController.java
+++ src/main/java/com/cedrus/patpetillo/springkafkapingpong/controller/PingPongBallController.java
@@ -5,7 +5,6 @@ import com.cedrus.patpetillo.springkafkapingpong.model.*;
 import com.cedrus.patpetillo.springkafkapingpong.stream.PingPongBallService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
